d5b2050e7aece7db03984916a9417832828d538d
kboot2022 <kboot42@gmail.com>
2024-02-15 18:47:38 -0500

    update matlab script for short week

--- matlab_scripts/precip_processing.m
+++
@@ -1,7 +0,0 @@
-% Processing netCDF precipitation download
-
-path = '/Users/kboothomefolder/OneDrive - UNC-Wilmington/x_consult/precip/data'
-
-% Example file ncdd-195101-grd-scaled.nc
-
-ncdisp('/Users/kboothomefolder/OneDrive-UNC-Wilmington/x_consult/precip/data/ncdd-195101-grd-scaled.nc')
