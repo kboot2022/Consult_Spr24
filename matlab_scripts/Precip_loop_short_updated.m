cb7c9f1b52b298a58d122d0a637a3d76207f827f
kboot2022 <kboot42@gmail.com>
2024-02-15 19:56:46 -0500

    updated matlab full loop

--- matlab_scripts/Precip_loop_short_updated.m
+++ matlab_scripts/Precip_loop_short_updated.m
@@ -10,7 +10,7 @@ mask=ncread(fname,'mask'); % O's and 1's grid
 [Lonmask,Latmask]=meshgrid(lon,lat);
 mask=mask';
 
-%% ORIG CODE: Concatenate precip through files - one year
+% ORIG CODE: Concatenate precip through files - one year
 
 prcp_ann=[];
 
@@ -32,7 +32,7 @@ latnc=ncread(ncf,'lat');
 Lonnc=Lonnc';
 Latnc=Latnc';
 
-%% 14-day accumulation at each observation point
+% 14-day accumulation at each observation point
 
 % need to add sum of leftover days to account for days 
 % non-divisible by 14.
@@ -68,99 +68,4 @@ for biwk=14:size(prcp_ann,3)
 end
 
 save('/Users/kboothomefolder/Library/CloudStorage/OneDrive-UNC-Wilmington/x_consult/precip/prcp_poly_14day_1951.mat', 'prcp_poly_14day');
-%}
-
-%%
-
-%{
-% 14 day accum -- Running total volume 
-
-%Initialize storage dataframes
-P_results=NaN(length(mask_X),nt);
-P_results_meters=NaN(length(mask_X),nt);
-P_results_volume=NaN(length(mask_X),nt);
-grid_element_area=NaN(size(mask_X));
-
-dlat = abs(mask_lat(2) - mask_lat(1)); % constant value 1 degree
-earth_rad = 6371000; %meters -- assumes a spherical earth
-dlat_meters = dlat * pi * earth_rad / 180;
-dlon = 1;
-dlon_meters=NaN(size(mask_X));
-
-for imask=1:nmask
-    latitude_at_grid = (mask_Y(imask)); 
-    dlon_meters(imask) = dlon * pi * earth_rad * cosd(latitude_at_grid)/ 180;
-    grid_element_area(imask) = dlon_meters(imask) * dlat_meters;
-    for iday=14:nt
-        P_results(imask,iday)=sum(P_results_daily(imask,iday-13:iday));
-        P_results_meters(imask,iday) = P_results(imask,iday)/1000;
-        for i = 1:length(grid_element_area)
-            P_results_volume(imask, iday) =  grid_element_area(i) * P_results_meters(imask, iday);
-        end
-    end
-end
-
-save('/Users/kboothomefolder/Library/CloudStorage/OneDrive-UNC-Wilmington/x_consult/precip/P_results_volume_1951.mat', 'P_results_volume');
-
-%}
-%%
-
-%{
-% Add to the reference date
-ref_date = datenum('1800-01-01 00:00:00', 'yyyy-mm-dd HH:MM:SS'); % Add t days to the reference date; 
-
-% convert to double type for and add to datestr t.
-date = double(ref_date + t); nt = length(date);
-
-%datestr(date); 
-%}
-
-%% 14 day accum -- Section
-
-%{
-i14 = 1;
-nt14 = round(nt/14); % Total days divided by 14
-P_results_section14=NaN(length(mask_X),nt14);
-
-for imask=1:nmask
-    for i = 1:round(length(P_results_daily)/14)
-        P_results_section14(imask,i) = sum(P_results_daily(i14:i14+13), 'omitnan');
-        i14 = i14+14;
-    end 
-end
-%}
-
-%% Concat precip for full dataset - incomplete
-%{
-% initialize prcp_ann to store 1 year data
-prcp_ann=[];
-year_range=[1951:1:2021]; % set number of years here
-
-for i=1:length(year_range); 
-    yr = year_range(i);
-    oy=num2str(yr);
-
-    for mon=1:2; % set number of months here 
-
-        % construct reference year and month for each file
-        om=num2str(100+mon); % format to get 0 before the month
-        om=num2str(om(2:3)); % get the 1 out of month char
-        oyom=[oy om];
-
-        % Update file path
-        ncf=['/Users/kboothomefolder/OneDrive_UNCW/x_consult/precip/data/ncdd-', oyom, '-grd-scaled.nc'];
-        %ncf %test file name by printing
-
-        % read precipitation variable
-        prcp=ncread(ncf,'prcp'); 
-        %prcp_mon=sum(prcp,3);
-        
-        % concatenate to end of prcp_ann
-        %prcp_ann=cat(3,prcp_ann,prcp);
-        %prcp_anncat(# of dimensions, prcp_ann, prcp data)
-        
-    end
-
-end
-%}
 
